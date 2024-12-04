8d0b72e57dee18b91e3882945b108e615c2103e9
Aryan Sharma <aryansharma@WINDELLAP-733.local>
2024-12-05 01:42:31 +0530

    added the dist folder

---
+++ dist/cjs/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "VWOScript", {
+  enumerable: true,
+  get: function get() {
+    return _component.VWOScript;
+  }
+});
+var _component = require("./component.js");
---
+++ dist/types/component.d.ts
@@ -0,0 +1,9 @@
+export function VWOScript({ accountId, type, settingsTimeout, hideElement, hideElementStyle, scriptAttributes }: {
+    accountId: any;
+    type?: string | undefined;
+    settingsTimeout?: number | undefined;
+    hideElement?: string | undefined;
+    hideElementStyle?: string | undefined;
+    scriptAttributes?: {} | undefined;
+}): React.JSX.Element | null;
+import React from 'react';
