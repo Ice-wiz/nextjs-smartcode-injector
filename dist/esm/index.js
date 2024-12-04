9e7a468fa3280ab292a78f2ccacb3c27c3168010
Aryan Sharma <aryansharma@WINDELLAP-733.local>
2024-12-05 03:42:37 +0530

    remove dist files

--- dist/esm/index.js
+++
@@ -1,1 +0,0 @@
-export { VWOScript } from './component.js';
--- dist/types/component.d.ts
+++
@@ -1,9 +0,0 @@
-export function VWOScript({ accountId, type, settingsTimeout, hideElement, hideElementStyle, scriptAttributes }: {
-    accountId: any;
-    type?: string | undefined;
-    settingsTimeout?: number | undefined;
-    hideElement?: string | undefined;
-    hideElementStyle?: string | undefined;
-    scriptAttributes?: {} | undefined;
-}): React.JSX.Element | null;
-import React from 'react';
