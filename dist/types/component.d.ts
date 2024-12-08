4d692b858e5e3940f870cd5e7e55a2cc7c515ee0
Aryan Sharma <aryansharma@WINDELLAP-733.local>
2024-12-08 13:05:18 +0530

    added version to vwoScript

--- dist/types/component.d.ts
+++ dist/types/component.d.ts
@@ -1,9 +1,10 @@
-export function VWOScript({ accountId, type, settingsTimeout, hideElement, hideElementStyle, scriptAttributes }: {
+export function VWOScript({ accountId, type, settingsTimeout, hideElement, hideElementStyle, scriptAttributes, version }: {
     accountId: any;
     type?: string | undefined;
     settingsTimeout?: number | undefined;
     hideElement?: string | undefined;
     hideElementStyle?: string | undefined;
     scriptAttributes?: {} | undefined;
+    version?: number | undefined;
 }): React.JSX.Element | null;
 import React from 'react';
